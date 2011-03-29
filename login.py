6c17f1a467b9ba6d5dbf5ed5d518420c7946fc7a
Parker Moore <parkrmoore@gmail.com>
2011-03-29 14:27:39 -0700

    rough addition in login.py

--- login.py
+++ login.py
@@ -1,9 +1,11 @@
-# /usr/bin/python
-
-print "Content-Type: text/html\n\n"
-
-from session import Session
-import cgi
+#!/usr/bin/env python
+try:
+        import traceback, sys, os, cgi
+        # The following makes errors go to HTTP client's browser
+        # instead of the server logs.
+        sys.stderr = sys.stdout
+        from session import Session
+        import cgi
 
 form = cgi.FieldStorage()
 username = str(form.getvalue("username"))
@@ -20,3 +22,18 @@ print u.is_logged_in()
 #print bottom of page
 with open("bottom.html.pyt", "r") as bottom:
 	print bottom.read()
+
+        
+except Exception, e:
+        print 'Content-type: text/html\n'
+        print
+        print '&lt;html&gt;&lt;head&gt;&lt;title&gt;'
+        print str(e)
+        print '&lt;/title&gt;'
+        print '&lt;/head&gt;&lt;body&gt;'
+        print '&lt;h1&gt;TRACEBACK&lt;/h1&gt;'
+        print '&lt;pre&gt;'
+        traceback.print_exc()
+        print '&lt;/pre&gt;'
+        print '&lt;/body&gt;&lt;/html&gt;'
+print "Content-Type: text/html\n\n"
