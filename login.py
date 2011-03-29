b96879d5b1da78e9b6c0d3beb18c60c03db0eeca
Parker Moore <parkrmoore@gmail.com>
2011-03-29 17:29:48 -0400

    proper indentation in login.py

--- login.py
+++ login.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python
 try:
-        import traceback, sys, os, cgi
-        # The following makes errors go to HTTP client's browser
-        # instead of the server logs.
-        sys.stderr = sys.stdout
-        from session import Session
-        import cgi
+	import traceback, sys, os, cgi
+	# The following makes errors go to HTTP client's browser
+	# instead of the server logs.
+	sys.stderr = sys.stdout
+	from session import Session
 
-form = cgi.FieldStorage()
-username = str(form.getvalue("username"))
-password = str(form.getvalue("password"))
-usertype = str(form.getvalue("usertype"))
+	form = cgi.FieldStorage()
+	username = str(form.getvalue("username"))
+	password = str(form.getvalue("password"))
+	usertype = str(form.getvalue("usertype"))
 
-#print standard header
-with open("header.html.pyt", "r") as header:
-	print header.read() % ("Login")
+	#print standard header
+	with open("header.html.pyt", "r") as header:
+		print header.read() % ("Login")
 
-u = Session(username, password, usertype)
-print u.is_logged_in()
+	u = Session(username, password, usertype)
+	print u.is_logged_in()
 
-#print bottom of page
-with open("bottom.html.pyt", "r") as bottom:
-	print bottom.read()
-
+	#print bottom of page
+	with open("bottom.html.pyt", "r") as bottom:
+		print bottom.read()
         
 except Exception, e:
         print 'Content-type: text/html\n'
@@ -36,4 +34,4 @@ except Exception, e:
         traceback.print_exc()
         print '&lt;/pre&gt;'
         print '&lt;/body&gt;&lt;/html&gt;'
-print "Content-Type: text/html\n\n"
+
