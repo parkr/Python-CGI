0b29d200bc6b03d7cad90927b785649206a4859b
Parker Moore <parkrmoore@gmail.com>
2011-03-29 16:18:17 -0400

    added session module

---
+++ login.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python
+
+import cgi
+
+class Session:
+
+	def __init__(self, username, password, usertype):
+		self.username = username
+		self.password = password
+		self.usertype = usertype
+		self.logged_in = 0
+		# load file
+		with open("users.usv", "r") as f:
+			counter = 0
+			possible = str(self.username)+str(self.password)+str(self.usertype)+"\n"
+			print possible
+			for line in f:
+				counter += 1
+				print line
+				if possible == line:
+					print "You should be logged in"
+					self.logged_in = 1
+			print 'file read (', counter, "lines )"
+
+
+	def is_logged_in(self):
+		return self.logged_in
+
