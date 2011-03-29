b32644fac59b10fd9f3e2d5537a1b2206d4b9590
Parker Moore <parkrmoore@gmail.com>
2011-03-29 18:37:30 -0400

    renamed self valuables with preceding underscore

--- session.py
+++ session.py
@@ -4,52 +4,52 @@ class Session:
 
 	def __init__(self, username=None, password=None, usertype=None):
 		if username != None and password != None and usertype != None:
-			self.username = username
-			self.password = password
-			self.usertype = usertype
-			self.logged_in = 0
+			self._username = username
+			self._password = password
+			self._usertype = usertype
+			self._logged_in = 0
 			# load file
 			f = open("users.usv", "r")
 			counter = 0
-			possible = str(self.username)+str(self.password)+str(self.usertype)+"\n"
+			possible = str(self._username)+str(self._password)+str(self._usertype)+"\n"
 			for line in f.readlines():
 				counter += 1
 				if possible == line:
-					self.logged_in = 1
-			if self.logged_in:
-				self.login_message = str("You have been logged in!")
+					self._logged_in = 1
+			if self._logged_in:
+				self._login_message = str("You have been logged in!\n")
 			else:
-				self.login_message = str("You have not been logged in.")
-			num_entries = str("("+str(counter)+" entries in the database)")
-			self.login_message += str(num_entries)
+				self._login_message = str("You have not been logged in.\n")
+			num_entries = str("("+str(counter)+" entries in the database)\n")
+			self._login_message += str(num_entries)
 		else:
-			self.login_message = str("Error: wrong arguments")
-		self.html = "false"
+			self._login_message = str("Error: wrong arguments\n")
 
 	def login_message(self):
-		if self.html == "true":
-			return self.login_message.replace("\n", "<br />\n")
+		if self._html == "true":
+			output = self._login_message
+			return output.replace("\n", "<br />\n")
 		else:
-			return self.login_message
+			return self._login_message
 
 	def html(self, value):
-		self.html = value
+		self._html = value
 	
 	def is_logged_in(self):
-		return self.logged_in
+		return self._logged_in
 
 	def get_usertype(self):
-		return self.usertype
+		return self._usertype
 
 	def get_username(self):
-		return self.username
+		return self._username
 
 	def __str__(self):
-		if self.html == "true":
+		if self._html == "true":
 			tab = "&nbsp;&nbsp;&nbsp;&nbsp;"
 			output = "<pre>[<br />"
-			output += (tab+self.username+",<br />"+tab+self.password+",<br />"+tab+self.usertype+"<br />")
+			output += (tab+self._username+",<br />"+tab+self._password+",<br />"+tab+self._usertype+"<br />")
 			output += "]</pre>"
 			return output
 		else:
-			return "[username='"+self.username+"', password='"+self.password+"', usertype='"+self.usertype+"']"
+			return "[username='"+self._username+"', password='"+self._password+"', usertype='"+self._usertype+"']"
